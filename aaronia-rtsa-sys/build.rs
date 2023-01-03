5a865b0a54eee72ccc034bea10d3cb924aee8567
Bastian Bloessl <mail@bastibl.net>
2023-01-03 21:55:11 +0100

    sys: improve build script

--- aaronia-rtsa-sys/build.rs
+++ aaronia-rtsa-sys/build.rs
@@ -1,17 +1,40 @@
-use std::path::PathBuf;
 use std::env;
+use std::path::PathBuf;
+
+const LIB: &str = "AaroniaRTSAAPI";
+const LIB_NAME: &str = "libAaroniaRTSAAPI.so";
+const HEADER_NAME: &str = "aaroniartsaapi.h";
+
+fn search() -> Option<PathBuf> {
+    let paths = env::var_os("RTSA_DIR")
+        .unwrap_or(concat!(env!("HOME"), "/Aaronia/RTSA/Aaronia-RTSA-Suite-PRO").into());
+
+    for dir in env::split_paths(&paths) {
+        let lib_path = dir.join(LIB_NAME);
+        let inc_path = dir.join(HEADER_NAME);
+        if lib_path.is_file() && inc_path.is_file() {
+            return Some(dir);
+        }
+    }
+    None
+}
 
 fn main() {
-    println!("cargo:rustc-link-search=/home/basti/Aaronia/RTSA/Aaronia-RTSA-Suite-PRO/");
-    println!("cargo:rustc-link-lib=AaroniaRTSAAPI");
+    let dir = search().unwrap();
+
+    println!("cargo:rustc-link-search={}", dir.to_str().unwrap());
+    println!("cargo:rustc-link-lib={LIB}");
     println!("cargo:rerun-if-changed=wrapper.h");
 
     let bindings = bindgen::Builder::default()
         .clang_arg("-x")
         .clang_arg("c++")
         .clang_arg("-std=c++14")
-        .clang_arg("-I/home/basti/Aaronia/RTSA/Aaronia-RTSA-Suite-PRO/")
+        .clang_arg(format!("-I{}", dir.to_str().unwrap()))
         .header("wrapper.h")
+        .allowlist_function("AARTSAAPI.*")
+        .allowlist_var("AARTSAAPI.*")
+        .allowlist_type("AARTSAAPI.*")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .generate()
         .expect("Unable to generate bindings");
@@ -21,4 +44,3 @@ fn main() {
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
 }
-
