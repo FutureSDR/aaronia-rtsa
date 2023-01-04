a1e7ed0b6517ebce28df0a6e120ad3833dbb1930
Bastian Bloessl <mail@bastibl.net>
2023-01-04 01:51:49 +0100

    wrapping

--- examples/rx.rs
+++ examples/rx.rs
@@ -1,5 +1,17 @@
+use aaronia_rtsa::handle;
+use aaronia_rtsa::init;
+use aaronia_rtsa::shutdown;
 use aaronia_rtsa::version;
+use aaronia_rtsa::Memory;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("RTSA library version: {}", version());
+
+    init(Memory::Medium)?;
+    {
+        let _h = handle()?;
+    }
+    shutdown()?;
+
+    Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,14 @@ pub struct Handle {
     inner: sys::AARTSAAPI_Handle,
 }
 
+impl Drop for Handle {
+    fn drop(&mut self) {
+        unsafe {
+            res(sys::AARTSAAPI_Close(&mut self.inner)).expect("error dropping API handle");
+        }
+    }
+}
+
 pub struct Packet {
     inner: sys::AARTSAAPI_Packet,
 }
@@ -61,38 +69,164 @@ pub enum Memory {
 impl From<u32> for Memory {
     fn from(value: u32) -> Self {
         match value {
-            1 => Memory::Medium,
-            2 => Memory::Large,
-            3 => Memory::Ludicrous,
+            sys::AARTSAAPI_MEMORY_MEDIUM => Memory::Medium,
+            sys::AARTSAAPI_MEMORY_LARGE => Memory::Large,
+            sys::AARTSAAPI_MEMORY_LUDICROUS => Memory::Ludicrous,
             _ => Memory::Small,
+        }
+    }
+}
+
+impl From<Memory> for u32 {
+    fn from(value: Memory) -> Self {
+        match value {
+            Memory::Small => sys::AARTSAAPI_MEMORY_SMALL,
+            Memory::Medium => sys::AARTSAAPI_MEMORY_MEDIUM,
+            Memory::Large => sys::AARTSAAPI_MEMORY_LARGE,
+            Memory::Ludicrous => sys::AARTSAAPI_MEMORY_LUDICROUS,
         }
     }
 }
 
 #[derive(Debug, Clone)]
 pub struct PacketFlags {
-    v: u32,
+    v: u64,
 }
 
 impl PacketFlags {
-    pub fn new(v: u32) -> Self {
+    pub fn new(v: u64) -> Self {
         PacketFlags { v }
     }
     pub fn segment_start(&self) -> bool {
-        self.v & sys::AARTSAAPI_PACKET_SEGMENT_START != 0
+        self.v & sys::AARTSAAPI_PACKET_SEGMENT_START as u64 != 0
     }
     pub fn segment_end(&self) -> bool {
-        self.v & sys::AARTSAAPI_PACKET_SEGMENT_END != 0
+        self.v & sys::AARTSAAPI_PACKET_SEGMENT_END as u64 != 0
     }
     pub fn stream_start(&self) -> bool {
-        self.v & sys::AARTSAAPI_PACKET_STREAM_START != 0
+        self.v & sys::AARTSAAPI_PACKET_STREAM_START as u64 != 0
     }
     pub fn stream_end(&self) -> bool {
-        self.v & sys::AARTSAAPI_PACKET_STREAM_END != 0
+        self.v & sys::AARTSAAPI_PACKET_STREAM_END as u64 != 0
+    }
+}
+
+pub type Result = std::result::Result<(), Error>;
+
+#[derive(Clone, Debug, thiserror::Error)]
+pub enum Error {
+    #[error("Empty")]
+    Empty,
+    #[error("Retry")]
+    Retry,
+
+    #[error("Idle")]
+    Idle,
+    #[error("Connecting")]
+    Connecting,
+    #[error("Connected")]
+    Connected,
+    #[error("Starting")]
+    Starting,
+    #[error("Running")]
+    Running,
+    #[error("Stopping")]
+    Stopping,
+    #[error("Disconnecting")]
+    Disconnecting,
+
+    #[error("Warning")]
+    Warning,
+    #[error("Warning Value Adjusted")]
+    WarningValueAdjusted,
+    #[error("Warning Value Disabled")]
+    WarningValueDisabled,
+
+    #[error("Error")]
+    Error,
+    #[error("Error Not Initialized")]
+    ErrorNotInitialized,
+    #[error("Error Not Found")]
+    ErrorNotFound,
+    #[error("Error Busy")]
+    ErrorBusy,
+    #[error("Error Not Open")]
+    ErrorNotOpen,
+    #[error("Error Not Connected")]
+    ErrorNotConnected,
+    #[error("Error Invalid Config")]
+    ErrorInvalidConfig,
+    #[error("Error Buffer Size")]
+    ErrorBufferSize,
+    #[error("Error Invalid Channel")]
+    ErrorInvalidChannel,
+    #[error("Error Invalid Parameter")]
+    ErrorInvalidParameter,
+    #[error("Error Invalid Size")]
+    ErrorInvalidSize,
+    #[error("Error Missing Paths File")]
+    ErrorMissingPathsFile,
+    #[error("Error Value Invalid")]
+    ErrorValueInvalid,
+    #[error("Error Value Malformed")]
+    ErrorValueMalformed,
+
+    #[error("Undocumented")]
+    Undocumented,
+}
+
+fn res(r: sys::AARTSAAPI_Result) -> Result {
+    match r {
+        0x00000000 => Ok(()),
+        0x00000001 => Err(Error::Empty),
+        0x00000002 => Err(Error::Retry),
+
+        0x10000000 => Err(Error::Idle),
+        0x10000001 => Err(Error::Connecting),
+        0x10000002 => Err(Error::Connected),
+        0x10000003 => Err(Error::Starting),
+        0x10000004 => Err(Error::Running),
+        0x10000005 => Err(Error::Stopping),
+        0x10000006 => Err(Error::Disconnecting),
+
+        0x40000000 => Err(Error::Warning),
+        0x40000001 => Err(Error::WarningValueAdjusted),
+        0x40000002 => Err(Error::WarningValueDisabled),
+
+        0x80000000 => Err(Error::Error),
+        0x80000001 => Err(Error::ErrorNotInitialized),
+        0x80000002 => Err(Error::ErrorNotFound),
+        0x80000003 => Err(Error::ErrorBusy),
+        0x80000004 => Err(Error::ErrorNotOpen),
+        0x80000005 => Err(Error::ErrorNotConnected),
+        0x80000006 => Err(Error::ErrorInvalidConfig),
+        0x80000007 => Err(Error::ErrorBufferSize),
+        0x80000008 => Err(Error::ErrorInvalidChannel),
+        0x80000009 => Err(Error::ErrorInvalidParameter),
+        0x8000000a => Err(Error::ErrorInvalidSize),
+        0x8000000b => Err(Error::ErrorMissingPathsFile),
+        0x8000000c => Err(Error::ErrorValueInvalid),
+        0x8000000d => Err(Error::ErrorValueMalformed),
+        _ => Err(Error::Undocumented),
     }
 }
 
 pub fn version() -> String {
     let n = unsafe { sys::AARTSAAPI_Version() };
     format!("{}.{}", n >> 16, n & 0xffff)
+}
+
+pub fn init(memory: Memory) -> Result {
+    unsafe { res(sys::AARTSAAPI_Init(memory.into())) }
+}
+
+pub fn shutdown() -> Result {
+    unsafe { res(sys::AARTSAAPI_Shutdown()) }
+}
+
+pub fn handle() -> std::result::Result<Handle, Error> {
+    let mut h = sys::AARTSAAPI_Handle {
+        d: std::ptr::null_mut(),
+    };
+    unsafe { res(sys::AARTSAAPI_Open(&mut h)).map(|_| Handle { inner: h }) }
 }
