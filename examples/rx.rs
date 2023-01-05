a84682b732fb77d72e86d7c3f126fa5ab25b42cf
Bastian Bloessl <mail@bastibl.net>
2023-01-05 23:15:17 +0100

    asdf

--- examples/rx.rs
+++ examples/rx.rs
@@ -17,22 +17,27 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     dev.config("device/outputformat", "iq")?;
     dev.config("device/receiverclock", "92MHz")?;
     dev.config("main/decimation", "1 / 64")?;
+    dev.config("main/centerfreq", "810e6")?;
+    dev.config("main/reflevel", "-20")?;
     dev.connect()?;
     dev.start()?;
 
-    rx(&mut dev)?;
+    let mut s = rx(&mut dev)?;
 
     dev.stop()?;
     dev.disconnect()?;
     dev.close()?;
 
+    plot(&mut s);
+
     Ok(())
 }
 
-fn rx(dev: &mut Device) -> Result<(), aaronia_rtsa::Error> {
-    const N: usize = 8192;
+const N: usize = 8192 * 2;
+fn rx(dev: &mut Device) -> Result<[Complex32; N], aaronia_rtsa::Error> {
     let mut samples = [Complex32::new(0.0, 0.0); N];
     let mut i = 0;
+
     while i < N {
         let p = dev.packet()?;
         let cur = p.samples();
@@ -42,31 +47,11 @@ fn rx(dev: &mut Device) -> Result<(), aaronia_rtsa::Error> {
         dev.consume()?;
     }
 
-    plot(&mut samples);
-    Ok(())
+    Ok(samples)
 }
 
 fn plot(s: &mut [num_complex::Complex32]) {
     use gnuplot::*;
-    let re = s.iter().map(|s| s.re);
-    let im = s.iter().map(|s| s.im);
-
-    let mut fg = Figure::new();
-
-    fg.axes2d()
-        .set_title("Samples", &[])
-        .lines(
-            0..s.len(),
-            re,
-            &[LineWidth(3.0), Color("brown"), LineStyle(DotDash)],
-        )
-        .lines(
-            0..s.len(),
-            im,
-            &[LineWidth(3.0), Color("blue"), LineStyle(DotDash)],
-        );
-
-    fg.show().unwrap();
 
     let mut planner = rustfft::FftPlanner::new();
     planner.plan_fft_forward(s.len()).process(s);
@@ -74,15 +59,10 @@ fn plot(s: &mut [num_complex::Complex32]) {
     let abs = s.iter().map(|s| s.norm_sqr().log10());
 
     let mut fg = Figure::new();
-
-    fg.axes2d()
-        .set_title("Spectrum", &[])
-        .lines(
-            0..s.len(),
-            abs,
-            &[LineWidth(3.0), Color("blue"), LineStyle(DotDash)],
-        );
-
+    fg.axes2d().set_title("Spectrum", &[]).lines(
+        0..s.len(),
+        abs,
+        &[LineWidth(3.0), Color("blue"), LineStyle(DotDash)],
+    );
     fg.show().unwrap();
-
 }
